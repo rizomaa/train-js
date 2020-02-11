607d2ebb416dfbae6ec77eacd3639c3401895a02
Ryzomych <rizoma@mail.riri>
2020-02-11 15:18:38 +0300

    more features for forkify (completed)

--- forkify/starter/src/js/index.js
+++ forkify/starter/src/js/index.js
@@ -1,10 +1,12 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
 import List from './models/List';
+import Likes from './models/Likes';
 import * as searchView from './views/searchView';
-import { elements, renderLoader, clearLoader } from './views/base';
 import * as recipeView from './views/recipeView';
-
+import * as listView from './views/listView';   
+import * as likesView from './views/likesView';
+import { elements, renderLoader, clearLoader } from './views/base';
 
 
 /* Global state of the App
@@ -18,10 +20,8 @@ const state = {};
 /** SEARCH CONTROLLER */
 const controlSearch = async () => {
     //1. get a query from the view.
-    const query = searchView.getInput(); //TODO
-    
-  // console.log(query);
-    
+    const query = searchView.getInput(); 
+        
     if (query) {
         //2. A new search object and add to the state.
      
@@ -53,7 +53,7 @@ elements.searchForm.addEventListener('submit', e => {
 });
 
 elements.searchResPages.addEventListener('click', e => {
-    const btn = e.target.closest('.btn-inline')
+    const btn = e.target.closest('.btn-inline');
     
     if (btn) {
         searchView.clearResults();
@@ -65,11 +65,16 @@ elements.searchResPages.addEventListener('click', e => {
 });
 
 /** RECIPE CONTROLLER */
+
+
+//for testing 
+
+
 const controlRecipe = async () => {
     
     // Get ID from URL
     const id = window.location.hash.replace('#', '');
-    console.log(id);
+    //console.log(id);
     
     if (id) {
         //1. Prepare UI for changes.
@@ -97,15 +102,96 @@ const controlRecipe = async () => {
 
             //5. Render Recipe on UI
             clearLoader();
-            recipeView.renderRecipe(state.recipe);
+            recipeView.renderRecipe(
+                state.recipe, 
+                state.likes.isLiked(id)
+            );
             
             //console.log(state.recipe);
             
         } catch (error) {
+           // console.log(error);               
             alert('Error processing recipe');
         }
     }
 };
+
+/* LIST CONTROLLER */
+
+const controlList = () => {
+    
+    //Creating new list if there is not yet.
+    if (!state.list) state.list = new List();
+    
+    //Add each ingredient to the list and UI
+    state.recipe.ingredients.forEach(el => {
+        const item = state.list.addItem(el.count, el.count, el.ingredient);
+        listView.renderItem(item);
+    });
+};
+
+/*LIKES CONTROLLER*/
+
+const controlLike = () => {
+    if (!state.likes) state.likes = new Likes();
+    
+    const currentID = state.recipe.id;
+    
+    //User has no liked recipe yet.
+    if (!state.likes.isLiked(currentID)) {
+        
+        //Add like from the state (data)
+        const newLike = state.likes.addLike(
+            currentID, 
+            state.recipe.title, 
+            state.recipe.author, 
+            state.recipe.img
+        );
+        
+        //Toggle the like buttun
+        likesView.toggleLikeBtn(true);
+        
+        //Add the Like from the UI list
+        likesView.renderLike(newLike);
+        
+        //console.log(state.likes);
+        
+    // User already have liked recipe
+    } else {
+        
+        //Remove like to the state (data)
+        state.likes.deleteLike(state.recipe.id);
+        
+        //Toggle the like buttun
+        likesView.toggleLikeBtn(false);
+        
+        //Remove the Like to the UI list
+        likesView.deleteLike(currentID);
+    }
+    likesView.toggleLikeMenu(state.likes.getNumLikes());
+};
+
+//Handle delete and update list item events
+elements.shopping.addEventListener('click', e => {
+    const id = e.target.closest('.shopping__item').dataset.itemid;
+    
+    //Handle the Delete button
+    if (e.target.matches('.shopping__delete, .dataset.itemid *')) {
+        
+        //Delete from state
+        state.list.deleteItem(id);
+        
+        //Delete from UI
+        listView.deleteItem(id);
+        
+    //Handle count update
+    } else if (e.target.matches('.shopping__count-value')) {
+        const val = parseFloat(e.target.value, 10);
+        state.list.updateCount(id, val);
+    }
+    
+    //Handle the item change
+});
 
 /*
 window.addEventListener('hashchange', controlRecipe);
@@ -113,6 +199,22 @@ window.addEventListener('load', controlRecipe);
 */
 
 ['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
+
+
+//Restore liked recipes on page load 
+window.addEventListener('load', () => {
+    state.likes = new Likes();
+    
+    //restore likes
+    state.likes.readStorage();
+    
+    //Toggle the like menu button
+    likesView.toggleLikeMenu(state.likes.getNumLikes());
+    
+    //Render the existing likes (our Recipes in the menu)
+    state.likes.likes.forEach(like => likesView.renderLike(like));
+});
+
 
 //Handling recipe button clicks
 
@@ -127,10 +229,23 @@ elements.recipe.addEventListener('click', e => {
         //Increase if button is clicked
         state.recipe.updateServings('inc');
         recipeView.updateServingsIngredients(state.recipe);
+    } else if (e.target.matches('.recipe__btn--add, .recipe__btn--add *')) {
+        //Add ingredients to shopping list
+        controlList();
+    } else if (e.target.matches('.recipe__love, .recipe__love *')) {
+        controlLike();
+        //console.log(e.target);
     }
-    console.log(state.recipe);
+    
+    /*else if (e.target.matches('.')) {
+        likesView.renderLike();
+    }*/
+               
+    
+    //console.log(state.recipe);
 });
+
+
 
 window.l = new List();
 //.addItem(state.recipe.ingredient.count);
-
--- forkify/starter/src/js/views/searchView.js
+++ forkify/starter/src/js/views/searchView.js
@@ -11,7 +11,7 @@ export const clearResults = () => {
     elements.searchResPages.innerHTML = '';
 }
 
-const limitRecipeTitle = (title, limit = 17) => {
+export const limitRecipeTitle = (title, limit = 17) => {
     
     const newTitle = [];
     if (title.length > limit) {
@@ -71,7 +71,7 @@ export const highlightSelected = id => {
         el.classList.remove('results__link--active');
     });
     
-    document.querySelector(`a[href*="${id}"]`).classList.add('results__link--active');
+    document.querySelector(`.results__link[href*="${id}"]`).classList.add('results__link--active');
 } 
 
 export const renderResults = (recipes, page = 1, resPerPage = 10) => {
